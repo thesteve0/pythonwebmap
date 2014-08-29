1c6df54894f3fb9fb7a874e40c117c710dc89c46
Steven Pousty <spousty@redhat.com>
2014-08-29 16:35:52 -0700

    just backing up

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,3 +1,12 @@
 __author__ = 'spousty'
 
+infile = open('US_CONCISE.csv', 'r')
+outfile = open('gnis.json', 'w')
 
+print(infile.readline())
+for line in infile:
+    splitline = line.rstrip().split(',')
+    print splitline
+
+
+print "The End"
