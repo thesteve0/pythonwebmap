b249187f0f8eb7ae13f942adc0f6a2342bec2280
Steven Pousty <spousty@redhat.com>
2014-08-29 16:25:09 -0700

    just backing up

--- ws.py
+++ ws.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from flask import g
+import os
+from pymongo.mongo_client import MongoClient
+
+#TODO import data (maybe new data)
+#todo build 2d spehere index
+#todo try a simple spatial query
+#todo do a call that maps to /ws/parks and returns all
 
 app = Flask(__name__)
 #add this so that flask doesn't swallow error messages
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
+def get_db_client():
+    if not hasattr(g, 'mongodb_client'):
+        g.mongodb_client = get_MongoDB()
+    return g.mongodb_client
+
+
+def get_MongoDB():
+    client = MongoClient(os.environ['OPENSHIFT_NOSQL_DB_URL'])
 
 @app.route("/")
 def base():
