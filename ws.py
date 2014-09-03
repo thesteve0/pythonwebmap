a5b2d1cc084859a24fba88317583d6bdcefd8ebb
Steven Pousty <spousty@redhat.com>
2014-09-02 20:07:37 -0700

    Deploy

--- ws.py
+++ ws.py
@@ -12,18 +12,33 @@ app = Flask(__name__)
 #add this so that flask doesn't swallow error messages
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-def get_db_client():
-    if not hasattr(g, 'mongodb_client'):
+def get_db():
+    if not hasattr(g, 'mongodb_db'):
         g.mongodb_client = get_MongoDB()
     return g.mongodb_client
 
 
 def get_MongoDB():
-    client = MongoClient(os.environ['OPENSHIFT_NOSQL_DB_URL'])
+    client = MongoClient(os.environ['OPENSHIFT_MONGODB_DB_URL'])
+
+    return client
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+if hasattr(g, 'mongodb_client'):
+    g.mongodb_client.close()
+
 
 @app.route("/")
 def base():
-    return "<strong>hello from Flask and IDEA </strong>"
+    client = get_db()
+    db = client['OPENSHIFT_APP_NAME']
+    placenames = db['placenames']
+
+    result = placenames.find_one()
+
+    return str(result)
 
 @app.route("/test")
 def test():
