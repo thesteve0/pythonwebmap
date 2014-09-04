33f44d753f96464e24e6356325a843ff415c6b70
Steven Pousty <spousty@redhat.com>
2014-09-04 09:37:58 -0700

    Deploy

--- ws.py
+++ ws.py
@@ -8,14 +8,13 @@ import json
 from bson import json_util
 from bson import objectid
 
-#TODO import data (maybe new data)
-#todo build 2d spehere index
-#todo try a simple spatial query
-#todo do a call that maps to /ws/parks and returns all
-
 app = Flask(__name__)
 #add this so that flask doesn't swallow error messages
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+##############################################
+## Database setup
+#############################################
 
 def get_db():
     if not hasattr(g, 'mongodb_client'):
@@ -35,6 +34,25 @@ def close_db(error):
 if hasattr(g, 'mongodb_client'):
     g.mongodb_client.close()
 
+#############################################
+## Real Queries
+##############################################
+
+#find points within a bounding box
+@app.route("/ws/parks/within")
+def within():
+    db = get_db()
+
+    #Get the box - production code would trap for invalid values
+    lat1 = float(request.args.get('lat1'))
+    lon1 = float(request.args.get('lon1'))
+    lat2 = float(request.args.get('lat2'))
+    lon2 = float(request.args.get('lon2'))
+
+    # make a geoJSON box
+    geometry = { "type" : "Polygon", "coordinates" : [[[lon1, lat1], [lon2, lat1], [lon2, lat2], [lon1, lat2], [lon1, lat1]]]}
+    result = db.placenames.find({"pos" : { "$geoWithin" : { "$geometry" : geometry} } })
+    return Response(response=str(json.dumps({'results':list(result)},default=json_util.default)), status=200, mimetype="application/json" )
 
 
 #find parks near a lat and long passed in as query parameters (near?lat=45.5&lon=-82)
@@ -42,29 +60,28 @@ if hasattr(g, 'mongodb_client'):
 def near():
     #setup the connection
     db = get_db()
-    #get the request parameters
+    #get the request parameters - production code would trap for invalid values
     lat = float(request.args.get('lat'))
     lon = float(request.args.get('lon'))
-    #use the request parameters in the query
-    print "before query " + str(lat) + " :: " + str(lon)
-    result = db.placenames.find({"pos" : { "$near" : {"$geometry" : { "type" : "Point" , "coordinates": [ lon , lat ] }}}}).limit(100)
-    #result = db.placenames.count({"pos" : { "$near" : {"$geometry" : { "type" : "Point" , "coordinates": [ lon , lat ] }}}})
-    print "after query"
 
-    #for doc in result:
-    #   print doc
+    #use the request parameters in the query
+    #again limiting results here due to data size
+    result = db.placenames.find({"pos" : { "$near" : {"$geometry" : { "type" : "Point" , "coordinates": [ lon , lat ] }}}}).limit(200)
+
     #turn the results into valid JSON
     return Response(response=str(json.dumps({'results':list(result)},default=json_util.default)), status=200, mimetype="application/json" )
-    #return str(result)
+
 
 @app.route("/ws/parks")
-def AllParks():
+def all_parks():
     db = get_db()
     #only return the first 100 docs
-    result = db.placenames.find().limit(100)
+    result = db.placenames.find().limit(200)
     return Response(response=str(json.dumps({'results':list(result)},default=json_util.default)), status=200, mimetype="application/json" )
 
-    #return str(json.dumps({'results':list(result)},default=json_util.default))
+#########################################
+## Just Testing basics
+########################################
 
 @app.route("/test")
 def test():
